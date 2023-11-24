7b81fc288e9d37def5f847798d97b9454eda7af0
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-11-24 18:34:29 +0530

    fix: update package name

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 
-	D1 "github.com/ghoshRitesh12/AoC_22_Go/Day1"
+	"github.com/ghoshRitesh12/AoC_22_Go/Day1"
 )
 
 func main() {
 	fmt.Println("I don't know what the heck I am doing, maybe experimenting and learning Golang")
 	
-	D1.A("./Day1/a.input.txt")
+	// Day1.A("./Day1/input.txt")
+	Day1.B("./Day1/input.txt")
 }
