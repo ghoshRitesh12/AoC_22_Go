dd7402d902a1a915e3619211285dfe87b9490190
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-11-25 12:39:38 +0530

    feat: add my own solution

--- Day2/b.go
+++ Day2/b.go
@@ -4,9 +4,55 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Q https://adventofcode.com/2022/day/2#part2
+
+func getAllMaps() (map[string] int, map[string] string, map[string] int, map[string] string) {
+	outcome := map[string] int {
+		"rock rock": 3,
+		"rock paper": 6,
+		"rock scissor": 0,
+
+		"paper paper": 3,
+		"paper rock": 0,
+		"paper scissor": 6,
+
+		"scissor scissor": 3,
+		"scissor paper": 0,
+		"scissor rock": 6,
+	}
+	lettersMap := map[string] string {
+		"A": "rock",
+		"B": "paper",
+		"C": "scissor",
+
+		"X": "rock",
+		"Y": "paper",
+		"Z": "scissor",
+	}
+	scoresMap := map[string] int {
+		"X": 1, // rock
+		"Y": 2, // paper
+		"Z": 3, // scissor
+	}
+	conditionalWin := map[string] string {
+		"AX": "Z",
+		"AY": "X",
+		"AZ": "Y",
+
+		"BX": "X",
+		"BY": "Y",
+		"BZ": "Z",
+
+		"CX": "Y",
+		"CY": "Z",
+		"CZ": "X",
+	}
+
+	return outcome, lettersMap, scoresMap, conditionalWin
+}
 
 func B(filepath string) {
 	file, err := os.Open(filepath);
@@ -21,21 +67,21 @@ func B(filepath string) {
 		fmt.Println("Scanner error: ", scannerErr)
 		return
 	}
-	scores := map[string] int {
-		"B X": 1, 
-		"C X": 2, 
-		"A X": 3, 
-		"A Y": 4,
-		"B Y": 5,
-		"C Y": 6,
-		"C Z": 7,
-		"A Z": 8,
-		"B Z": 9,
-	}
-	totalScore := 0
 
-	for scanner.Scan() {		
-		totalScore += scores[scanner.Text()]	
+	totalScore := 0
+	outcome, lettersMap, scoresMap, conditionalWin := getAllMaps()
+
+	for scanner.Scan() {
+		splittedInput := strings.Split(scanner.Text(), " ")
+		elfPick := splittedInput[0]
+		myPick := splittedInput[1]
+
+		conditionWinArg := conditionalWin[elfPick+myPick]
+
+		shapeScore := scoresMap[conditionWinArg]
+		roundScore := outcome[lettersMap[elfPick] + " " + lettersMap[conditionWinArg]]
+		
+		totalScore += (shapeScore + roundScore)
 	}
 
 	fmt.Println("")
